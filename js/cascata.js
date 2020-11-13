5175e5c7d8a4411afb8e7bbd4b154b84dce3b9d3
lucaandaloro <luca.andaloro@gmail.com>
2020-11-13 10:52:19 +0100

    Update cascata.js

--- js/cascata.js
+++ js/cascata.js
@@ -3,12 +3,12 @@
 //Crea il corretto cubo in base all'altezza
 function creaCascata() {
     var waters =[];
-    for(var i=0; i<9; i++){
+    for(var i=0; i<12; i++){
         for(var j = 0; j<5; j++){
             var geometryCube =new THREE.BoxGeometry(1,1,1);
             var meshWaters = new THREE.Mesh(geometryCube, getMateriale("water"));
-            meshWaters.position.x = -18-(i*0.4);
-            meshWaters.position.y = 14-i;
+            meshWaters.position.x = -17-(i*0.5);
+            meshWaters.position.y = 16-i;
             meshWaters.position.z = 4+j;
             scene.add(meshWaters);
 
