64f3f2a491d10cefd95f9c31ea16b086fe09d22f
lucaandaloro <luca.andaloro@gmail.com>
2020-11-15 16:15:24 +0100

    Update ponte.js

--- js/ponte.js
+++ js/ponte.js
@@ -1,34 +1,37 @@
 // --------------- FUNZIONE PER LA CREAZIONE DEL PONTE -----------
 
-function ponte(posX, posY, posZ){
+function creaPonte(){
 	var ponte = new THREE.Object3D();
-	var pavimento = new THREE.Object3D();
 
 	//pavimento
-	var w = 2;
-	var h = 0.6;
-	var d = 2;
+	var x = 15;
+	var y = 7;
+	var z = -8;
 	var yPrec;
-	for(var i = 0; i < 7; i++){
-			var geometriaPavimento = new THREE.BoxGeometry(w,h,d);
-            var material = new THREE.MeshPhongMaterial({
-                color: ("brown")
-            });
-			var meshPavimento = new THREE.Mesh(geometriaPavimento, material);	
-		if( i <= 3){
-			meshPavimento.position.y += i/2;
-			meshPavimento.position.z += i*1.5;
-			yPrec = meshPavimento.position.y;
-			
-		}else{
-			meshPavimento.position.y = yPrec - 0.5; 
-			meshPavimento.position.z += i*1.5;
-			yPrec = meshPavimento.position.y;
+	var geometriaPavimento = new THREE.BoxGeometry(3,0.6,2);
+	var meshPavimento = new THREE.Mesh(geometriaPavimento, getMateriale("mulino"));	
+	for(var i = 0; i < 12; i++){
+		var salita = meshPavimento.clone();
+		if(i<2){
+			salita.position.set(x,y+i,z-i);
+			salita.rotation.x = 45 * Math.PI/180;
+		}else if(i>2 && i<4){
+			salita.position.set(x,y+(i/1.5),z-(i/1.2));
+			salita.rotation.x = 25 * Math.PI/180;
+		}else if(i>4 && i<6){
+			salita.position.set(x,y+(i/2),z-(i/1.2));
+			salita.rotation.x = 10 * Math.PI/180;
+		}else if(i>6 && i<8){
+			salita.position.set(x,y+(i/2.9),z-(i/1.2));
+			salita.rotation.x = -10 * Math.PI/180;
+		}else if(i>8 && i<10){
+			salita.position.set(x,y+(i/5),z-(i/1.2));
+			salita.rotation.x = -30 * Math.PI/180;
+		}else if(i>10){
+			salita.position.set(x,y+(i/15),z-(i/1.2));
+			salita.rotation.x = -40 * Math.PI/180;
 		}
-		pavimento.add(meshPavimento);
+		ponte.add(salita);
 	}
-	
-	ponte.add(pavimento);
-	ponte.position.set(posX, posY, posZ);
 	scene.add(ponte);
 }
