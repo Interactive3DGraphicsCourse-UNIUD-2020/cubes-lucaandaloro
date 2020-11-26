e9b0ec6593e0939b6be3f1c107cf3157c1e9706c
lucaandaloro <luca.andaloro@gmail.com>
2020-11-26 22:16:07 +0100

    Update nuvole.js

--- js/nuvole.js
+++ js/nuvole.js
@@ -1,14 +1,16 @@
 // --------------- FUNZIONI PER LA CREAZIONE DELLE NUVOLE -----------
 
+// Parte di codice preso dal seguqnete esempio https://discourse.threejs.org/t/clouds-of-cubes/3218
+
 function createBoxWithRoundedEdges(width, height, depth, radius0, smoothness) {
-    let shape = new THREE.Shape();
-    let eps = 0.00001;
-    let radius = radius0 - eps;
+    var shape = new THREE.Shape();
+    var eps = 0.00001;
+    var radius = radius0 - eps;
     shape.absarc(eps, eps, eps, -Math.PI / 2, -Math.PI, true);
     shape.absarc(eps, height - radius * 2, eps, Math.PI, Math.PI / 2, true);
     shape.absarc(width - radius * 2, height - radius * 2, eps, Math.PI / 2, 0, true);
     shape.absarc(width - radius * 2, eps, eps, 0, -Math.PI / 2, true);
-    let geometry = new THREE.ExtrudeBufferGeometry(shape, {
+    var geometry = new THREE.ExtrudeBufferGeometry(shape, {
       depth: depth - radius0 * 2,
       bevelEnabled: true,
       bevelSegments: smoothness * 2,
